dfe1883327fe98a561cb20a92a4595f49a7c8e29
rudrashisdutta <rudra2001official@gmail.com>
2021-07-11 03:39:17 +0530

    Initial commit
    11-07-2021 03-39a
    
    Additionally a .gitignore file was created to ignore certain unnecessary files and folders if found.

---
+++ PRACTICE/index.js
@@ -0,0 +1,2 @@
+//this is my first javascript
+console.log('HELLO WORLD!');
---
+++ PRACTICE/main.js
@@ -0,0 +1,7 @@
+var name="POOJA";
+console.log("HELLO "+name+"!");
+console.log("\n"+(3+4));
+if(confirm("Are you Pooja ?"))
+{
+    console.log("Heyyy "+name);
+}
